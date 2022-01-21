abec78aa1285930cb83bf10fd6a924f295dcf8e9
fuyunwang <2375872953@qq.com>
2022-01-21 15:05:00 +0800

    add algo

---
+++ src/main/java/com/gopher/leetcode/Code7.java
@@ -0,0 +1,20 @@
+package com.gopher.leetcode;
+
+/**
+ * @Title Code7
+ * @Author fyw
+ * @Date 2022/1/21 14:53
+ * @Description:
+ */
+public class Code7 {
+    public int reverse(int x) {
+        int result=0;
+        while (x!=0){
+            if (result>0&&result >(Integer.MAX_VALUE- x%10)/10||result<0&&result<(Integer.MIN_VALUE-x%10)/10)
+                return 0;
+            result = result * 10 + x % 10;
+            x/=10;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/gopher/leetcode/tree/TreeModel2.java
@@ -0,0 +1,10 @@
+package com.gopher.leetcode.tree;
+
+/**
+ * @Title TreeModel2
+ * @Author fyw
+ * @Date 2022/1/21 15:04
+ * @Description:
+ */
+public class TreeModel2 {
+}
