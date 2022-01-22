fa225df57c53fd2aeaaec6bef3058829b30bc354
fuyunwang <2375872953@qq.com>
2022-01-22 22:21:23 +0800

    add algo

---
+++ src/main/java/com/gopher/leetcode/Code19.java
@@ -0,0 +1,26 @@
+package com.gopher.leetcode;
+
+/**
+ * @Title Code19
+ * @Author fyw
+ * @Date 2022/1/22 22:14
+ * @Description:
+ */
+public class Code19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int count=0;
+        ListNode temp=new ListNode(-1);
+        temp.next=head;
+        ListNode cur=temp;
+        while(cur.next!=null){
+            count++;
+            cur=cur.next;
+        }
+        cur=temp;
+        for(int i=0;i<count-n;i++){
+            cur=cur.next;
+        }
+        cur.next=cur.next.next;
+        return temp.next;
+    }
+}
