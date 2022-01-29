68e1d1cafdb6b6690dfc9829048cfbbce4b520d7
fuyunwang <2375872953@qq.com>
2022-01-29 22:30:51 +0800

    add algo

---
+++ src/main/java/com/gopher/leetcode/greed/Code55.java
@@ -0,0 +1,18 @@
+package com.gopher.leetcode.greed;
+
+/**
+ * @Title Code55
+ * @Author fyw
+ * @Date 2022/1/29 22:01
+ * @Description: 跳跃游戏
+ */
+public class Code55 {
+    public boolean canJump(int[] nums) {
+        int end=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (end<i) return false;// 说明跳不过去
+            end=Math.max(end,i+nums[i]);
+        }
+        return true;
+    }
+}
