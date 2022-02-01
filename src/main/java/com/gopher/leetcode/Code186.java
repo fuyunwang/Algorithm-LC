69d895c3ccb31de5fb9697a6b65e42cd63fd2f5f
fuyunwang <2375872953@qq.com>
2022-02-01 20:46:10 +0800

    add algo

---
+++ src/main/java/com/gopher/leetcode/Code186.java
@@ -0,0 +1,39 @@
+package com.gopher.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Title Code186
+ * @Author fyw
+ * @Date 2022/2/1 20:25
+ * @Description: 翻转字符串里的单词 II
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ * 输入: ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
+ * 输出: ["b","l","u","e"," ","i","s"," ","s","k","y"," ","t","h","e"]
+ */
+public class Code186 {
+    public void reverseWords(char[] s) {
+        reverse(s,0,s.length-1);
+        int i=0;
+        int pre=i;
+        for (; i <= s.length;) {
+            if (i<s.length&&s[i]==' '||i==s.length){
+                reverse(s,pre,i-1);
+                pre=i+1;
+                i++;
+            }else{
+                i++;
+            }
+        }
+    }
+    void reverse(char[] chars,int l,int r){
+        while (l<r){
+            char temp=chars[l];
+            chars[l]=chars[r];
+            chars[r]=temp;
+            l++;
+            r--;
+        }
+    }
+}
--- src/main/java/com/gopher/leetcode/Code199.java
+++ src/main/java/com/gopher/leetcode/Code199.java
@@ -3,18 +3,34 @@ package com.gopher.leetcode;
 import com.gopher.leetcode.tree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Title Code199
  * @Author fyw
  * @Date 2022/2/1 19:48
- * @Description:
+ * @Description: 二叉树的右视图，前序遍历利用map中同一个键的值会覆盖的特点来存储每一层的数据
  */
 public class Code199 {
     public List<Integer> rightSideView(TreeNode root) {
         if (root==null)
             return new ArrayList<>();
-
+        Map<Integer,Integer> map=new HashMap<>();
+        recur(root,0,map);
+        List<Integer> res=new ArrayList<>();
+        for (Integer i:map.values()){
+            res.add(i);
+        }
+        return res;
+    }
+    void recur(TreeNode root, int deep, Map<Integer,Integer> map){
+        if (root==null)
+            return;
+        map.put(deep,root.val);
+        // 参数deep其实是作为key存在
+        recur(root.left,deep+1,map);
+        recur(root.right,deep+1,map);
     }
 }
