7ec0f8b45b0cc1f7fbf8b12453bfbe76734cebcf
fuyunwang <2375872953@qq.com>
2022-02-10 21:18:32 +0800

    add algo

--- src/main/java/com/gopher/leetcode/order/or40/Code33.java
+++ src/main/java/com/gopher/leetcode/order/or40/Code33.java
@@ -4,7 +4,34 @@ package com.gopher.leetcode.order.or40;
  * @Title Code33
  * @Author fyw
  * @Date 2022/2/8 21:16
- * @Description: 搜索旋转数组
+ * @Description: 搜索旋转排序数组
+ * 如果中间的数小于最右边的数，则右半段是有序的，若中间数大于最右边数，则左半段是有序的，
+ * 我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
  */
 public class Code33 {
+    public int search(int[] nums, int target) {
+        int len=nums.length;
+        int left=0,right=len-1;
+        while (left<=right){
+            int mid=(left+right)/2;
+            if (nums[mid]==target)
+                return mid;
+            if (nums[mid]<nums[right]){
+                //右半段有序
+                if (nums[mid]<target&&target<=nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
+            }else{
+                if (nums[left]<=target&&target<nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+
+            }
+        }
+        return -1;
+    }
 }
--- src/main/java/com/gopher/leetcode/order/or40/Code34.java
+++ src/main/java/com/gopher/leetcode/order/or40/Code34.java
@@ -4,8 +4,34 @@ package com.gopher.leetcode.order.or40;
  * @Title Code34
  * @Author fyw
  * @Date 2022/2/2 16:56
- * @Description:
- * 高频题目
+ * @Description: 在排序数组中查找元素的第一个和最后一个位置
+ * 高频题目，即二分，左右边界的二分、
  */
 public class Code34 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res=new int[]{-1,-1};
+        res[0]=search(nums,target,true);
+        res[1]=search(nums,target,false);
+        return res;
+    }
+    int search(int[] nums,int target,boolean isLeft){
+        int left=0,right=nums.length-1;
+        int res=-1;
+        while (left<=right){            // 只有左闭右开时不取等号其他的都取
+            int mid=(left+right)/2;
+            if (nums[mid]<target){
+                left=mid+1;
+            }else if (nums[mid]>target){
+                right=mid-1;
+            }else{
+                res=mid;
+                if (isLeft){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
+        }
+        return res;
+    }
 }
