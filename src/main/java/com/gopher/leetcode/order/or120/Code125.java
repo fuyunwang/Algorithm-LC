ea5e7800f56c73da3beb1f8806250b42eed9ae24
fuyunwang <2375872953@qq.com>
2022-02-12 21:32:41 +0800

    add algo

--- src/main/java/com/gopher/leetcode/order/or120/Code125.java
+++ src/main/java/com/gopher/leetcode/order/or120/Code125.java
@@ -11,10 +11,21 @@ public class Code125 {
         int l=0,r=s.length()-1;
         final char[] chars = s.toCharArray();
         while (l<r){
-//            Character.is
-            if (Character.toLowerCase(chars[l++])!=Character.toLowerCase(chars[r++])){
+            char lc=chars[l];
+            char rc=chars[r];
+            if(!Character.isLetterOrDigit(lc)){
+                l++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(rc)){
+                r--;
+                continue;
+            }
+            if (Character.toLowerCase(lc)!=Character.toLowerCase(rc)){
                 return false;
             }
+            l++;
+            r--;
         }
         return true;
     }
