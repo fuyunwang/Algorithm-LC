34461a2ab300307d1eaf6fb1312c9eaba7bf5f6b
fuyunwang <2375872953@qq.com>
2022-02-20 18:32:24 +0800

    add algo

--- src/main/java/com/gopher/coding/Binary.java
+++ src/main/java/com/gopher/coding/Binary.java
@@ -20,4 +20,15 @@ public class Binary {
             System.out.print(x>>i&1);
         }
     }
+
+    class LeetCode191{
+        public int hammingWeight(int n) {
+            int res=0;
+            while (n!=0){
+                n-=(n&-n);
+                res++;
+            }
+            return res;
+        }
+    }
 }
