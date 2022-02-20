3d82040a6dfae3a93faced4ee0c9b0c901dca37b
fuyunwang <2375872953@qq.com>
2022-02-20 20:48:04 +0800

    add algo

--- src/main/java/com/gopher/leetcode/order/or60/Code56.java
+++ src/main/java/com/gopher/leetcode/order/or60/Code56.java
@@ -8,7 +8,8 @@ import java.util.List;
  * @Title Code56
  * @Author fyw
  * @Date 2022/1/31 20:54
- * @Description:
+ * @Description: 合并区间
+ * 贪心，按区间左端点排序
  */
 public class Code56 {
     public int[][] merge(int[][] intervals) {
@@ -20,11 +21,11 @@ public class Code56 {
         Arrays.sort(intervals,(x,y)->x[0]-y[0]);
         int start=intervals[0][0], end=intervals[0][1];
         for (int i = 1; i < n; i++) {
-            if (intervals[i][0]>end){
+            if (intervals[i][0]>end){   // 严格大于
                 ans.add(new int[]{start,end});
                 start=intervals[i][0];
                 end=intervals[i][1];
-            }else{
+            }else{                      // 其他情况必有交集，延长右端点
                 end=Math.max(end,intervals[i][1]);
             }
         }
