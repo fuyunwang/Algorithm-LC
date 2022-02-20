11e63cf3a4d8a7da3cf60cfef0d4d961162a9ff7
fuyunwang <2375872953@qq.com>
2022-02-20 17:55:17 +0800

    add algo

--- src/main/java/com/gopher/coding/DoublePointer.java
+++ src/main/java/com/gopher/coding/DoublePointer.java
@@ -17,7 +17,7 @@ public class DoublePointer {
             int res = 0;
             for(int i = 0, j = 0; i < n;i++) {
                 char c=s.charAt(i);
-                map.put(c, map.getOrDefault(c, 0) + 1); //nums[i]不存在返回0
+                map.put(c, map.getOrDefault(c, 0) + 1);
                 while(map.get(c) > 1) {
                     map.put(s.charAt(j), map.get(s.charAt(j)) - 1);
                     j++;
