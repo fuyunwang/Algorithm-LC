a2f4f9507437706189bd05d8f9ae840ca2b9557f
fuyunwang <2375872953@qq.com>
2022-02-22 21:58:40 +0800

    add algo

--- src/main/java/com/gopher/interview/Code86.java
+++ src/main/java/com/gopher/interview/Code86.java
@@ -1,10 +1,56 @@
 package com.gopher.interview;
 
+import java.util.Arrays;
+
 /**
  * @Title Code86
  * @Author fyw
  * @Date 2022-02-22 11:18
  * @Description 数独检查
+ * LeetCode36
  */
 public class Code86 {
+    public boolean isValidSudoku(char[][] board) {
+        int n = 9;
+        boolean[] st = new boolean[10];
+        //判断行
+        for(int i = 0;i < n;i ++){
+            Arrays.fill(st,false);
+            for(int j = 0;j < n;j ++){
+                if(board[i][j] != '.'){
+                    int t = board[i][j] - '0';
+                    if(st[t]) return false;
+                    st[t] = true;
+                }
+            }
+        }
+        //判断列
+        for(int i = 0;i < n;i ++){
+            Arrays.fill(st,false);
+            for(int j = 0;j < n;j ++) {
+                if(board[j][i] != '.') {
+                    int t = board[j][i] - '0';
+                    if(st[t]) return false;
+                    st[t] = true;
+                }
+            }
+        }
+        //判断九宫格
+        for(int i = 0;i < n;i += 3){
+            for(int j = 0;j < n;j += 3){
+                Arrays.fill(st,false);
+                for(int u = 0;u < 3;u ++) {
+                    for(int v = 0;v < 3;v ++) {
+                        if(board[i + u][j + v] != '.') {
+                            int t = board[i + u][j + v] - '0';
+                            if(st[t]) return false;
+                            st[t] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
 }
--- src/main/java/com/gopher/interview/Code87.java
+++ src/main/java/com/gopher/interview/Code87.java
@@ -1,10 +1,42 @@
 package com.gopher.interview;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * @Title Code87
  * @Author fyw
  * @Date 2022-02-22 11:18
  * @Description 蒙斯特
+ * 插入排序
  */
 public class Code87 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = 110;
+        String[] cards = new String[N];
+        int T = Integer.parseInt(in.readLine());
+        for (int cases = 1; cases <= T; cases ++) {
+            // 初始化
+            int n = Integer.parseInt(in.readLine());
+            for (int i = 0; i < n; i ++) {
+                cards[i] = in.readLine();
+            }
+
+            int res = 0;
+            for (int i = 1; i < n; i ++) {
+                if (cards[i].compareTo(cards[i - 1]) < 0) {
+                    for (int j = i; j > 0; j --) {
+                        if (cards[j].compareTo(cards[j - 1]) < 0) {
+                            String temp = cards[j];
+                            cards[j] = cards[j - 1];
+                            cards[j - 1] = temp;
+                        }
+                    }
+                    res ++;
+                }
+            }
+            System.out.printf("Case #%d: %d\n", cases, res);
+        }
+    }
 }
