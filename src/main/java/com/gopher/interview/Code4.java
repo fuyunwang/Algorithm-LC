47ac265ba3d933352739125027d58cc322eced94
Fuyunwang <2375872953@qq.com>
2022-02-22 10:06:02 +0800

    add algo

--- src/main/java/com/gopher/interview/Code4.java
+++ src/main/java/com/gopher/interview/Code4.java
@@ -7,4 +7,58 @@ package com.gopher.interview;
  * @Description: 寻找矩阵极小值，Code162
  */
 public class Code4 {
+    interface Getvalue{
+        int query(int x, int y);
+    }
+    class Solution implements Getvalue{
+        public int[] getMinimumValue(int n) {
+            int  lo = 0, hi = n-1;
+
+            while (lo < hi) {
+                int mid = lo + (hi - lo >> 1); // 注意这里，不要写成 lo + (hi - lo) >> 1，>>的优先级低
+
+                // 找到中间一列的最小值
+                int min = Integer.MAX_VALUE;
+                int minIndex = -1; // 行的索引
+                for (int i = 0; i < n; i++) {
+                    int temp = query(i, mid);
+                    if (min > temp) {
+                        min = temp;
+                        minIndex = i;
+                    }
+                }
+
+                // mid+1 一定存在，因为 mid 最大为 n-2, 但 mid-1 不一定存在，因为 mid 最小可以为0
+                if (mid <= 0) {
+                    lo = mid + 1;
+                } else if (query(minIndex, mid-1) > min && query(minIndex, mid+1) > min) {
+                    return new int[]{minIndex, mid};
+                }
+
+                if (query(minIndex, mid-1) < min) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+
+            // 找到最后一列（lo == hi）的最小值
+            int min = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for (int i = 0; i < n; i++) {
+                int temp = query(i, lo); // 这里用 lo 和 hi 一样
+                if (min > temp) {
+                    min = temp;
+                    minIndex = i;
+                }
+            }
+
+            return new int[]{minIndex, lo};
+        }
+
+        @Override
+        public int query(int x, int y) {
+            return 0;
+        }
+    }
 }
--- src/main/java/com/gopher/interview/prac/RingEntry.java
+++ src/main/java/com/gopher/interview/prac/RingEntry.java
@@ -1,11 +1,33 @@
 package com.gopher.interview.prac;
 
+import com.gopher.leetcode.linkedlist.ListNode;
+
 /**
  * @Title RingEntry
  * @Author fyw
  * @Date 2022-02-22 9:28
  * @Description 链表中环的入口节点
+ * 慢指针走x+y步，快指针走了2x+2y+x步
+ * 做法是快指针2步走慢指针1步走，相遇时，慢指针回到起点，快慢指针一起一步走。
  */
 public class RingEntry {
-    
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while (fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if (slow==fast){
+                // 从相遇节点开始，另从头和相遇节点开始同步运行
+                ListNode index1=head;
+                ListNode index2=fast;
+                while (index1!=index2){
+                    index1=index1.next;
+                    index2=index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
 }
