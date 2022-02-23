59b4c963d443df46a94f4c50ac2ffc81f70e3272
Fuyunwang <2375872953@qq.com>
2022-02-23 16:35:33 +0800

    add algo

--- src/main/java/com/gopher/leetcode/order/or120/Code131.java
+++ src/main/java/com/gopher/leetcode/order/or120/Code131.java
@@ -1,14 +1,53 @@
 package com.gopher.leetcode.order.or120;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @Title Code131
  * @Author fyw
  * @Date 2022/2/8 21:57
- * @Description:
+ * @Description: 分割回文串I
+ * 指数次方，分割方案
  */
 public class Code131 {
+    /**
+     * 回文串，递推.f[i,j]快速判断[i,j]是否是回文串
+     * @param s
+     * @return
+     */
+    List<List<String>> ans = new ArrayList<List<String>>();
+    List<String> path = new ArrayList<String>();
+    boolean[][] g;           // 快速判断是否是回文串
+    public List<List<String>> partition(String s) {
+        int n = s.length();
 
+        g= new boolean[n][n];
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j <= i; j ++){
+                if(i == j) g[i][j] = true;
+                else if(s.charAt(i) == s.charAt(j) && (j + 1 > i - 1 || g[j + 1][i - 1]))
+                    g[j][i] = true;
+            }
+        }
+
+        dfs(0, s);  // 从第0个位置开始搜索
+        return ans;
+    }
+
+    void dfs(int u, String s){
+        if(u == s.length()){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = u; i < s.length(); i ++){
+            if(g[u][i]){
+                path.add(s.substring(u, i + 1));
+                dfs(i + 1, s);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
 }
--- src/main/java/com/gopher/leetcode/order/or120/Code132.java
+++ src/main/java/com/gopher/leetcode/order/or120/Code132.java
@@ -1,10 +1,39 @@
 package com.gopher.leetcode.order.or120;
 
+import java.util.Arrays;
+
 /**
  * @Title Code132
  * @Author fyw
  * @Date 2022/2/9 14:47
- * @Description:
+ * @Description: 分割回文串II
+ * 分割的最少次数
  */
 public class Code132 {
+    // f[i]表示s[1~i]所有分割方案，属性是最小值
+    // 最后一段是k~i
+    // 预处理判断是否是回文串
+    public int minCut(String s) {
+        int n = s.length();
+        s = " " + s;
+        boolean[][] g = new boolean[n + 1][n + 1];
+        for(int i = 1;i <= n;i ++)
+            for(int j = 1;j <= i;j ++)
+            {
+                if(i == j) g[j][i] = true;
+                else if(s.charAt(i) == s.charAt(j) && (i - 1 < j + 1 || g[j + 1][i - 1]))
+                    g[j][i] = true;
+            }
+
+        int[] f = new int[n + 1];
+        Arrays.fill(f, 0x3f3f3f3f);
+        f[0] = 0;
+        for(int i = 1;i <= n;i ++)
+            for(int j = 1;j <= i;j ++)
+            {
+                if(g[j][i]) f[i] = Math.min(f[i], f[j - 1] + 1);
+            }
+
+        return f[n] - 1;
+    }
 }
