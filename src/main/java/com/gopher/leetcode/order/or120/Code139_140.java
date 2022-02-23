7566f45bd0372453126f8e0e0717f29b6d031fcd
Fuyunwang <2375872953@qq.com>
2022-02-23 18:36:31 +0800

    add algo

--- src/main/java/com/gopher/leetcode/order/or120/Code139_140.java
+++ src/main/java/com/gopher/leetcode/order/or120/Code139_140.java
@@ -1,6 +1,8 @@
 package com.gopher.leetcode.order.or120;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -31,6 +33,47 @@ public class Code139_140 {
 
     class Code140{
         // 返回所有划分方案
+        List<String> ans = new ArrayList<String>();
+        HashSet<String> set = new HashSet<String>();
+        int n;
+        boolean[] f;
+        void dfs(String s, int u, String path)
+        {
+            if(u == n + 1)
+            {
+                ans.add(path);
+                return ;
+            }
+            for(int i = u;i <= n;i ++)
+            {
+                String t = s.substring(u, i + 1);
+                if(f[i] && set.contains(t))
+                {
+                    String next = path.length() == 0 ? t : path + " " + t ;
+                    dfs(s, i + 1, next);
+                }
+            }
+        }
+        public List<String> wordBreak(String s, List<String> wordDict) {
+            //初始化
+            for(String t : wordDict)    set.add(t);
+            n = s.length();
+            s = " " + s;
+            f = new boolean[n + 10];
+            f[0] = true;
+
+            for(int i = 1;i <= n;i ++) {
+                for(int j = 1;j <= i;j ++) {
+                    String t = s.substring(j,i + 1);
+                    if(wordDict.contains(t)) f[i] = f[j - 1] || f[i];
+                }
+            }
+            if(!f[n]) return ans;
+
+
+            dfs(s, 1, "");
+            return ans;
+        }
 
     }
 }
