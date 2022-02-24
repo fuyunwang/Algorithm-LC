a8a0691a299f63f131de88e05186e329daf93ad2
Fuyunwang <2375872953@qq.com>
2022-02-24 21:08:32 +0800

    add algo

--- src/main/java/com/gopher/leetcode/order/or400/Code402.java
+++ src/main/java/com/gopher/leetcode/order/or400/Code402.java
@@ -1,10 +1,44 @@
 package com.gopher.leetcode.order.or400;
 
+import java.util.LinkedList;
+
 /**
  * @Title Code402
  * @Author fyw
  * @Date 2022/2/9 12:26
- * @Description:
+ * @Description: 移掉K位数字
+ * Code316
  */
 public class Code402 {
+    public String removeKdigits(String num, int k) {
+        LinkedList<Character> stack=new LinkedList<>();
+        int n=num.length();
+        char[] nums=num.toCharArray();
+        for (int i = 0; i < n; i++) {
+            char c = nums[i];
+            while (k>0&&!stack.isEmpty()&&stack.peekLast()>c){
+                stack.pollLast();
+                k--;
+            }
+            stack.addLast(c);
+        }
+        while (k>0&&!stack.isEmpty()){
+            stack.pollLast();
+            k--;
+        }
+        StringBuilder sb=new StringBuilder();
+        while (!stack.isEmpty()){
+            sb.append(stack.pollLast());
+        }
+        String res=sb.reverse().toString();
+        int index=0;
+        for (int i = 0; i < res.length(); i++) {
+            if (res.charAt(i)=='0'){
+                index++;
+            }else if (res.charAt(i)!='0'){
+                break;
+            }
+        }
+        return index==res.length()?"0":res.substring(index);
+    }
 }
--- src/main/java/com/gopher/leetcode/order/or400/Code405.java
+++ src/main/java/com/gopher/leetcode/order/or400/Code405.java
@@ -4,7 +4,20 @@ package com.gopher.leetcode.order.or400;
  * @Title Code405
  * @Author fyw
  * @Date 2022/2/9 14:08
- * @Description:
+ * @Description: 数字转换为十六进制数
  */
 public class Code405 {
+    // int 转 16进制，32位 4位一组
+    public String toHex(int num) {
+        //分成8组，每组4位  （取4位，删除4位）
+        if(num==0) return "0";
+        String hex = "0123456789abcdef";
+        StringBuilder sb = new StringBuilder();
+        while(num!=0){
+            sb.append(hex.charAt(num&0xf));
+            num>>>=4;  //无符号右移
+        }
+        return sb.reverse().toString();
+    }
+
 }
