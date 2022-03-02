642f7ae3a0d5a66649d33cbfbd5a03efe1e5bdc1
fuyunwang <2375872953@qq.com>
2022-03-02 12:04:00 +0800

    add algo

---
+++ src/main/java/com/gopher/leetcode/types/structure/monotonous_queue/Template1.java
@@ -0,0 +1,16 @@
+package com.gopher.leetcode.types.structure.monotonous_queue;
+
+/**
+ * @Title Template1
+ * @Author fyw
+ * @Date 2022/3/2 11:46
+ * @Description: 单调队列
+ */
+public class Template1 {
+    static class Code239{
+
+    }
+    static class Code1425{
+
+    }
+}
