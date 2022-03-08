5ddaa011396d7fcd478b0b56e58898528e542562
Fuyunwang <2375872953@qq.com>
2022-03-08 22:06:31 +0800

    add algo

--- src/main/java/com/gopher/leetcode/types/graph/BFS.java
+++ src/main/java/com/gopher/leetcode/types/graph/BFS.java
@@ -9,6 +9,70 @@ import java.util.*;
  * @Description
  */
 public class BFS {
+    static class Code773{
+        /**
+         * 8数码问题变形
+         * @param board
+         * @return
+         */
+        public int slidingPuzzle(int[][] board) {
+            int [][] target = new int[][]{{1,2,3},{4,5,0}};
+            if (changeToStr(board).equals(changeToStr(target))) return 0;
+            Queue<int[][]> q = new LinkedList<>();
+            q.add(board);
+            Map<String, Integer> dist = new HashMap<>();
+            dist.put(changeToStr(board), 0);
+            int direc[][] = new int[][]{{1,0},{-1, 0},{0, 1},{0, -1}};
+            while (!q.isEmpty()){
+                int[][] t = q.poll();
+                int x = 0, y = 0;//'0' 的位置
+                for (int i = 0; i < 2; i++){
+                    for (int j = 0; j < 3; j++){
+                        if (t[i][j] == 0){
+                            x = i; y = j;
+                        }
+                    }
+                }
+                for (int i = 0 ; i < 4; i++){
+                    int a = x + direc[i][0];
+                    int b = y + direc[i][1];
+                    if (a >= 0 && a < 2 && b >= 0 && b < 3){
+                        int[][] r = copy(t);
+                        int temp = r[x][y];
+                        r[x][y] = r[a][b];
+                        r[a][b] = temp;
+                        if (dist.getOrDefault(changeToStr(r), 0) == 0){
+                            dist.put(changeToStr(r), dist.getOrDefault(changeToStr(t), 0) + 1);
+                            if (changeToStr(r).equals(changeToStr(target))) return dist.get(changeToStr(r));
+                            q.add(r);
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+
+        int[][] copy (int[][] target){
+            int[][] b = new int[target.length][target[0].length];
+            for (int i = 0; i < target.length; i++){
+                for (int j = 0; j < target[0].length; j++){
+                    b[i][j] = target[i][j];
+                }
+            }
+            return b;
+        }
+
+        String changeToStr(int[][] board){
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < board.length; i++){
+                for (int j = 0; j < board[0].length; j++){
+                    sb.append(board[i][j]);
+                }
+            }
+            return sb.toString();
+        }
+
+    }
     static class Code1091{
         // 二进制矩阵中的最短路径，迷宫问题
         public static int shortestPathBinaryMatrix(int[][] grid) {
