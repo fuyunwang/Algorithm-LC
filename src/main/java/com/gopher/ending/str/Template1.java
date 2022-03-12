bac7f3f548613898f8782fc48df43476a4391efe
fuyunwang <2375872953@qq.com>
2022-03-12 22:40:04 +0800

    add algo

---
+++ src/main/java/com/gopher/ending/str/Template1.java
@@ -0,0 +1,10 @@
+package com.gopher.ending.str;
+
+/**
+ * @Title Template1
+ * @Author fyw
+ * @Date 2022/3/12 22:39
+ * @Description: 字符串哈希
+ */
+public class Template1 {
+}
--- src/main/java/com/gopher/ending/structure/trie/Template.java
+++ src/main/java/com/gopher/ending/structure/trie/Template.java
@@ -7,4 +7,47 @@ package com.gopher.ending.structure.trie;
  * @Description:
  */
 public class Template {
+    class Code726{
+        class Trie{
+            Trie[] children = new Trie[26];
+            int idx=-1;
+        }
+        Trie head;
+        public void insert(String str, int id) {
+            Trie root = head;
+            for (char ch : str.toCharArray()) {
+                int cc = ch - 'a';
+                if (root.children[cc] == null) {
+                    root.children[cc] = new Trie();
+                }
+                root = root.children[cc];
+            }
+            root.idx=id;
+        }
+
+        public int[] dfs(Trie trie, int len) {
+            int[] res = { trie.idx,len};
+            for (int i = 0; i < 26; i++) {
+                if (trie.children[i] != null && trie.children[i].idx != -1) {
+                    int[] tmp = dfs(trie.children[i], len + 1);
+                    if (res[1] < tmp[1]) {
+                        res = tmp;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public String longestWord(String[] words) {
+            head = new Trie();
+            for (int i = 0; i < words.length; i++) {
+                insert(words[i],i);
+            }
+            int[] res = dfs(head, 0);
+            if (res[0] == -1) {
+                return "";
+            }
+            return words[res[0]];
+        }
+    }
 }
