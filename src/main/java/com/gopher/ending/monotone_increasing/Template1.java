271a75fa1558b94054641f5209a1f94a2574a081
Fuyunwang <2375872953@qq.com>
2022-03-12 12:35:59 +0800

    add algo

---
+++ src/main/java/com/gopher/ending/monotone_increasing/Template1.java
@@ -0,0 +1,14 @@
+package com.gopher.ending.monotone_increasing;
+
+/**
+ * @Title Template1
+ * @Author fyw
+ * @Date 2022-03-12 12:27
+ * @Description 增量
+ */
+public class Template1 {
+    static class Code945{
+        // 增量
+        
+    }
+}
