8dee41ede67d8d5562fd4e5231e2ccf747f90119
fuyunwang <2375872953@qq.com>
2022-03-14 20:14:46 +0800

    add algo

---
+++ src/main/java/com/gopher/ending/dp/BagDp.java
@@ -0,0 +1,38 @@
+package com.gopher.ending.dp;
+import java.io.*;
+import java.util.*;
+/**
+ * @Title BagDp
+ * @Author fyw
+ * @Date 2022/3/14 19:32
+ * @Description:
+ */
+public class BagDp {
+    static class CodeWing1{
+        // 0-1背包
+        public static void main(String[] args) throws IOException{
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+            String[] str = reader.readLine().trim().split(" ");
+            int n=Integer.parseInt(str[0]);
+            int m=Integer.parseInt(str[1]);
+            int[][] matrix=new int[n+1][2];// 存放所有的信息，第一维度是体积、第二维度是价值
+            for(int i=1;i<=n;i++){
+                str=reader.readLine().trim().split(" ");
+                matrix[i][0]=Integer.parseInt(str[0]);
+                matrix[i][1]=Integer.parseInt(str[1]);
+            }
+
+            // 进行状态转移计算
+            int[] f=new int[m+1];
+            for (int i = 1; i <= n; i++) {
+                for (int j=m;j>=matrix[i][0];j--){
+                    f[j]=Math.max(f[j],f[j-matrix[i][0]]+matrix[i][1]);
+                }
+            }
+            System.out.println(f[m]);
+        }
+    }
+    static class CodeWing2{
+        // 完全背包
+    }
+}
---
+++ src/main/java/com/gopher/ending/dp/PathDp.java
@@ -0,0 +1,25 @@
+package com.gopher.ending.dp;
+
+/**
+ * @Title PathDp
+ * @Author fyw
+ * @Date 2022/3/14 20:13
+ * @Description: 路线型DP
+ */
+public class PathDp {
+    static class CodeWing1{
+        // 数字三角形
+    }
+    static class CodeWing2{
+        // 摘花生
+    }
+    static class CodeWing3{
+        // 最低通行费
+    }
+    static class CodeWing4{
+        // 方格取数
+    }
+    static class CodeWing5{
+       // 传纸条 
+    }
+}
