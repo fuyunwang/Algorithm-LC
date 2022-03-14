df22568504b1ec4a6f0ceaf953d16a17a80e277d
fuyunwang <2375872953@qq.com>
2022-03-14 22:12:57 +0800

    add algo

--- src/main/java/com/gopher/ending/dp/BagDp.java
+++ src/main/java/com/gopher/ending/dp/BagDp.java
@@ -34,5 +34,33 @@ public class BagDp {
     }
     static class CodeWing2{
         // 完全背包
+
+        /**
+         * f[i,j]表示从前i个物品中选，总体积不超过j的选择方案的价值最大值
+         * 考虑第i个物品选0个、1个、2个、。。。的情况
+         * @param args
+         * @throws IOException
+         */
+        public static void main(String[] args) throws IOException{
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+            String[] str = reader.readLine().trim().split(" ");
+            int n=Integer.parseInt(str[0]);
+            int m=Integer.parseInt(str[1]);
+            int[][] matrix=new int[n+1][2];// 存放所有的信息，第一维度是体积、第二维度是价值
+            for(int i=1;i<=n;i++){
+                str=reader.readLine().trim().split(" ");
+                matrix[i][0]=Integer.parseInt(str[0]);
+                matrix[i][1]=Integer.parseInt(str[1]);
+            }
+
+            // 进行状态转移计算
+            int[] f=new int[m+1];
+            for (int i = 1; i <= n; i++) {
+                for (int j=matrix[i][0];j<=m;j++){
+                    f[j]=Math.max(f[j],f[j-matrix[i][0]]+matrix[i][1]);
+                }
+            }
+            System.out.println(f[m]);
+        }
     }
 }
--- src/main/java/com/gopher/ending/dp/PathDp.java
+++ src/main/java/com/gopher/ending/dp/PathDp.java
@@ -1,5 +1,10 @@
 package com.gopher.ending.dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * @Title PathDp
  * @Author fyw
@@ -12,14 +17,74 @@ public class PathDp {
     }
     static class CodeWing2{
         // 摘花生
+        /**
+         * f[i,j]表示从[1,1]走到[i,j]的所有路线中能取到花生数的最大值。
+         * 状态计算考虑最后一个位置可以从[i-1,j]或者[i,j-1]转移过来
+         */
+        static int N=110;
+        static int[][] f=new int[N][N];
+        public static void main(String[] args) throws IOException{
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(reader.readLine().trim());
+            while(T-->0){
+                // 首先读入行和列
+                String[] str=reader.readLine().trim().split(" ");
+                int r=Integer.parseInt(str[0]);
+                int c=Integer.parseInt(str[1]);
+                for (int i = 1; i <= r; i++) {
+                    str = reader.readLine().trim().split(" ");
+                    for (int j = 1; j <= c; j++) {
+                        f[i][j]=Integer.parseInt(str[j-1]);
+                    }
+                }
+                // 进行状态转移计算
+                for (int i = 1; i <= r; i++) {
+                    for (int j = 1; j <= c; j++) {
+                        f[i][j]=Math.max(f[i-1][j],f[i][j-1])+f[i][j];
+                    }
+                }
+                System.out.println(f[r][c]);
+            }
+        }
     }
     static class CodeWing3{
-        // 最低通行费
+        // 最低通行费，多了一个限制必须在2n-1内走到(其实本质还是不能走回头路)，求最小值
+        // 本质还是求矩阵中一条路线权值之和的最小值
+        static int N=110;
+        static int[][] f=new int[N][N];
+        public static void main(String[] args) throws IOException{
+//            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+            Scanner scanner=new Scanner(System.in);
+            int n=scanner.nextInt();
+            int[][] g=new int[n+1][n+1];
+            for(int i=1;i<=n;i++){
+//                final String[] split = reader.readLine().trim().split(" ");
+                for (int j =1; j<= n; j++) {
+                    g[i][j]=scanner.nextInt();
+                }
+            }
+            // 进行状态转移计算，注意求最小值需要特判
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i==1&&j==1) f[i][j]=g[i][j];
+                    else{
+                        f[i][j]=Integer.MAX_VALUE;
+                        if (i>1)
+                            f[i][j]=Math.min(f[i][j],f[i-1][j]+g[i][j]);
+                        if (j>1){
+                            f[i][j]=Math.min(f[i][j],f[i][j-1]+g[i][j]);
+                        }
+                    }
+                }
+            }
+            System.out.println(f[n][n]);
+        }
     }
     static class CodeWing4{
         // 方格取数
+        // 还是从左上角走到右下角，不过是走两次，而且走过一次的格子不能再走
     }
     static class CodeWing5{
-       // 传纸条 
+       // 传纸条
     }
 }
