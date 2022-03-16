b6fb2f22a76101189cd31c5c8c93725b9c7146e3
fuyunwang <2375872953@qq.com>
2022-03-16 21:01:12 +0800

    add algo

--- src/main/java/com/gopher/ending/backtrack/Template1.java
+++ src/main/java/com/gopher/ending/backtrack/Template1.java
@@ -41,6 +41,52 @@ public class Template1 {
             }
         }
     }
+    static class CodeWing2{
+        // n皇后，列控制终止条件，枚举每一行
+        static int N=20;
+        static char[][] g=new char[N][N];
+        // 存储是否访问过的标记
+        static boolean[] col=new boolean[N];
+        static boolean[] udg=new boolean[N];
+        static boolean[] dg=new boolean[N];
+        public static void main(String[] args) {
+            Scanner scanner=new Scanner(System.in);
+            int n=scanner.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    g[i][j]='.';
+                }
+            }
+            dfs(0,n);
+        }
+        static void dfs(int y,int n){
+            if (y==n){
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        System.out.print(g[i][j]);
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+                return;
+            }
+            for (int x = 0; x < n; x++) {   // 在当前列的每一行进行皇后放置
+                if(!col[x]&&!udg[x+y]&&!dg[y-x+n]){
+                    col[x]=true;
+                    udg[x+y]=true;
+                    dg[y-x+n]=true;
+                    g[x][y]='Q';
+                    dfs(y+1,n);
+                    g[x][y]='.';
+                    col[x]=false;
+                    udg[x+y]=false;
+                    dg[y-x+n]=false;
+                }
+            }
+        }
+
+    }
+
 
     static class Code332{
         // 重新安排路程
--- src/main/java/com/gopher/ending/graph/BFS.java
+++ src/main/java/com/gopher/ending/graph/BFS.java
@@ -1,5 +1,8 @@
 package com.gopher.ending.graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -9,6 +12,56 @@ import java.util.*;
  * @Description:
  */
 public class BFS {
+    static class CodeWing1{
+        // 迷宫
+        static class Code1{
+            static int N=110;
+            static int[][] g=new int[N][N]; // 图
+            static int[][] dist=new int[N][N]; // 记录每一个点到起点的距离
+            static int n,m;
+            static Queue<int[]> queue=new LinkedList<>();
+            static int[] dx=new int[]{-1,0,1,0};
+            static int[] dy=new int[]{0,1,0,-1};
+            public static void main(String[] args) throws IOException {
+                BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+                String[] str=reader.readLine().trim().split(" ");
+                n=Integer.parseInt(str[0]);
+                m=Integer.parseInt(str[1]);
+                for (int i = 0; i < n; i++) {
+                    str=reader.readLine().trim().split(" ");
+                    for (int j = 0; j < m; j++) {
+                        g[i][j]=Integer.parseInt(str[j]);
+                    }
+                }
+                System.out.println(bfs());
+            }
+            static int bfs(){
+                for (int i = 0; i < n; i++) {
+                    Arrays.fill(dist[i],-1);
+                }
+                dist[0][0]=0;
+                queue.offer(new int[]{0,0});
+                while (!queue.isEmpty()){
+                    int[] top=queue.poll();
+                    for (int i = 0; i < 4; i++) {
+                        int a=top[0]+dx[i];
+                        int b=top[1]+dy[i];
+                        if (a<0||a>=n||b<0||b>=m||dist[a][b]!=-1||g[a][b]!=0)
+                            continue;
+                        queue.offer(new int[]{a,b});
+                        dist[a][b]=dist[top[0]][top[1]]+1;
+                    }
+                }
+                return dist[n-1][m-1];
+            }
+        }
+        // 八数码
+        static class Code2{
+            
+        }
+    }
+
+
     static class Code864{
         // 类似双端队列求 AcWing1131
     }
