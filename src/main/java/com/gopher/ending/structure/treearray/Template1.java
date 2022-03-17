7fd72789e2b663b4510c5ba6020271a2b7f107f7
fuyunwang <2375872953@qq.com>
2022-03-17 22:56:31 +0800

    add algo

---
+++ src/main/java/com/gopher/ending/structure/treearray/Template1.java
@@ -0,0 +1,13 @@
+package com.gopher.ending.structure.treearray;
+
+/**
+ * @Title Template1
+ * @Author fyw
+ * @Date 2022/3/17 22:29
+ * @Description:
+ */
+public class Template1 {
+    static class CodeWing1{
+
+    }
+}
--- src/main/java/com/gopher/ending/structure/unionset/Template1.java
+++ src/main/java/com/gopher/ending/structure/unionset/Template1.java
@@ -1,12 +1,69 @@
 package com.gopher.ending.structure.unionset;
 
+import javax.sound.midi.Soundbank;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  * @Title Template1
  * @Author fyw
  * @Date 2022/3/12 20:27
  * @Description:
+ * 并查集用于判断环，如果两个点在连线之前已经处于同一个连通块那么可以成环
  */
 public class Template1 {
+    static class CodeWing1{
+        // 格子游戏，计算第几行结束游戏，也就是第几行两个点在一个连通块
+        static int N=201*201+10;
+        static int n,m;
+        static int[] p=new int[N];
+        static int find(int x){
+            if (p[x]!=x)
+                p[x]=find(p[x]);
+            return p[x];
+        }
+        static int get(int x,int y){
+            return x*n+y;       // 返回二维索引对一维索引的映射
+        }
+        public static void main(String[] args) throws IOException {
+            BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+            String[] str1=reader.readLine().trim().split(" ");
+            n=Integer.parseInt(str1[0]);
+            m=Integer.parseInt(str1[1]);
+            for (int i = 0; i < n*n; i++) { // 编号索引进行映射
+                p[i]=i;
+            }
+            int res=0;
+            for (int i = 1; i <= m; i++) { // 读入M个操作
+                String[] str2=reader.readLine().trim().split(" ");
+                int x=Integer.parseInt(str2[0])-1;// 注意编号到索引的对应
+                int y=Integer.parseInt(str2[1])-1;
+                String d=str2[2];
+                int a=get(x,y);
+                int b;
+                if (d.equals("D"))
+                    b=get(x+1,y);
+                else
+                    b=get(x,y+1);
+
+                int pa=find(a);
+                int pb=find(b);
+
+                if (pa==pb){
+                    res=i;
+                    break;
+                }
+                p[pa]=pb;
+            }
+            if (res==0)
+                System.out.println("draw");
+            else
+                System.out.println(res);
+        }
+    }
+
     static class Code765{
         int[] p;
 
