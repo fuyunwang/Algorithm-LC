7ffc2dee1dd861a443b888acccbae0c715261994
Fuyunwang <2375872953@qq.com>
2022-03-18 15:51:36 +0800

    add algo

---
+++ src/main/java/com/gopher/ending/math/PrimeTemplate1.java
@@ -0,0 +1,10 @@
+package com.gopher.ending.math;
+
+/**
+ * @Title PrimeTemplate1
+ * @Author fyw
+ * @Date 2022-03-17 20:40
+ * @Description 质数和合数都是针对于大于等于2的数而言
+ */
+public class PrimeTemplate1 {
+}
