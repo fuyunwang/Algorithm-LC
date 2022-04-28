a52993e431d6d25421b8ec370730ea0e552ecaa5
fuyunwang <2375872953@qq.com>
2022-04-28 11:23:11 +0800

    update algo

--- src/main/java/com/gopher/leetcode/order/or10/Code10.java
+++ src/main/java/com/gopher/leetcode/order/or10/Code10.java
@@ -12,34 +12,36 @@ public class Code10 {
         if (s == null || p == null) {
             return false;
         }
+        int n=s.length();
+        int m=p.length();
+        s=" "+s;
+        p=" "+p;
         boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-        dp[0][0] = true;//dp[i][j] 表示 s 的前 i 个是否能被 p 的前 j 个匹配
-        for (int i = 0; i < p.length(); i++) { // here's the p's length, not s's
-            if (p.charAt(i) == '*' && dp[0][i - 1]) {
-                dp[0][i + 1] = true; // here's y axis should be i+1
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j)=='*'){
+                dp[0][j]=dp[0][j-2];
             }
         }
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < p.length(); j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {//如果是任意元素 或者是对于元素匹配
-                    dp[i + 1][j + 1] = dp[i][j];
+                    dp[i][j] = dp[i-1][j-1];
                 }
                 if (p.charAt(j) == '*') {
-                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {//如果p前一个元素和s当前元素不匹配
-                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {//如果前一个元素不匹配 且不为任意元素
+                        dp[i][j] = dp[i][j - 2];    // *匹配0个字符
                     } else {
-                        // 1次，多次，0次
-                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                        dp[i][j] = (dp[i-1][j] || dp[i][j-1] || dp[i][j - 2]);  // 匹配多个字符
                             /*
-                            dp[i][j] = dp[i-1][j] // 多个字符匹配的情况
-                            or dp[i][j] = dp[i][j-1] // 单个字符匹配的情况
-                            or dp[i][j] = dp[i][j-2] // 没有匹配的情况
+                                dp[i][j] = dp[i-1][j] // 多个字符匹配的情况
+                                or dp[i][j] = dp[i][j-1] // 单个字符匹配的情况
+                                or dp[i][j] = dp[i][j-2] // 没有匹配的情况
                              */
-
                     }
                 }
             }
         }
-        return dp[s.length()][p.length()];
+        return dp[n][m];
     }
 }
