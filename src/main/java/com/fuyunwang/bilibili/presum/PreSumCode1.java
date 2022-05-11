d3c1823b17052d0a9d2473d8c5dcc401f688e1a8
Fuyunwang <2375872953@qq.com>
2022-05-11 17:17:40 +0800

    add presum template

---
+++ src/main/java/com/fuyunwang/bilibili/presum/PreSumCode1.java
@@ -0,0 +1,28 @@
+package com.fuyunwang.bilibili.presum;
+
+/**
+ * @Title PreSumCode1
+ * @Author fyw
+ * @Date 2022-05-11 17:15
+ * @Description
+ */
+public class PreSumCode1 {
+    class Code926{
+
+    }
+    class Code930{
+
+    }
+    class Code1052{
+
+    }
+    class Code1074{
+
+    }
+    class Code1094{
+
+    }
+    class Code1190{
+        
+    }
+}
