75788ba39b711b41d0fb9fec1a82c8fd9c0e5b7c
fuyunwang <2375872953@qq.com>
2022-05-22 22:08:06 +0800

    update algo

--- src/main/java/com/fuyunwang/bilibili/order/hot100/Code206.java
+++ src/main/java/com/fuyunwang/bilibili/order/hot100/Code206.java
@@ -10,6 +10,26 @@ import com.gopher.leetcode.types.structure.linkedlist.ListNode;
  */
 public class Code206 {
     public ListNode reverseList(ListNode head) {
-
+        if (head==null||head.next==null)
+            return head;
+        ListNode cur=head;
+        ListNode pre=null;
+        ListNode next=null;
+        while (cur!=null){
+            next=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=next;
+        }
+        return pre;
+    }
+    public ListNode reverseList2(ListNode head){
+        if (head==null||head.next==null){
+            return head;
+        }
+        ListNode node=reverseList2(head.next);
+        head.next.next=head;
+        head.next=null;
+        return node;
     }
 }
