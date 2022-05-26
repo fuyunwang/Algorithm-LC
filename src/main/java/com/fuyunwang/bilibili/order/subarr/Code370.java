436f29157c9156277c38cd06a5e5a8ae50662f2b
Fuyunwang <2375872953@qq.com>
2022-05-26 10:17:37 +0800

    add subarr

--- src/main/java/com/fuyunwang/bilibili/order/subarr/Code370.java
+++ src/main/java/com/fuyunwang/bilibili/order/subarr/Code370.java
@@ -11,10 +11,16 @@ public class Code370 {
      * 经典差分数组应用
      */
     public int[] getModifiedArray(int length, int[][] updates) {
-        int[] res=new int[updates.length];
-        for (int i = 0; i < res.length; i++) {
+        int[] res=new int[length];
+        for (int i = 0; i < updates.length; i++) {
             res[updates[i][0]]+=updates[i][2];
-            if (updates)
+            if (updates[i][1]+1<res.length){
+                res[updates[i][1]+1]-=updates[i][2];
+            }
         }
+        for (int i = 1; i < res.length; i++) {
+            res[i]+=res[i-1];
+        }
+        return res;
     }
 }
